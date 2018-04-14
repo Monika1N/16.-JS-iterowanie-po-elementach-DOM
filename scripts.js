af30dda92c96065b4915edef535112b54bc2fd90
Monika N <monia.niemczyk1@gmail.com>
2018-04-14 13:41:38 +0200

    Prepare loop 'for' and use '.innerText'

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-var button = document.getElementsByClassName("button")
+var button = document.getElementsByClassName('button');
+console.log(button);
+console.log(button[0]);
+console.log(button[1]);
+console.log(button[2]);
+console.log(button[3]);
+
+for (var i = 0 ; i < button.length ; i++) {
+	var text = button[i].innerText;
+	console.log("Button: " + text);
+}
